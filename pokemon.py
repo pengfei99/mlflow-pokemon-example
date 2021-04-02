83a93a14d0a7beef80be8ba3d768f79441dc096a
pengfei <liu.pengfei@hotmail.fr>
2021-04-02 19:18:26 +0200

    hot fix on run ID not foundd

--- pokemon.py
+++ pokemon.py
@@ -21,7 +21,7 @@ def get_model_accuracy(confusion_matrix):
 
 
 def mlflow_record(n_estimator, max_depth, min_samples_split):
-    remote_server_uri = "http://pengfei.org:8000"  # set to your server URI
+   # remote_server_uri = "http://pengfei.org:8000"  # set to your server URI
     mlflow.set_tracking_uri(remote_server_uri)
     mlflow.set_experiment("pokemon-experiment")
     with mlflow.start_run():
