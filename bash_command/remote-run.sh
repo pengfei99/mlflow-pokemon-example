06403ae5749eb59a15021f29d374999e67d452f9
pengfei <liu.pengfei@hotmail.fr>
2021-04-12 18:21:37 +0200

    correct run command for git remote run

--- bash_command/remote-run.sh
+++ bash_command/remote-run.sh
@@ -1,1 +1,6 @@
-mlflow run git@github.com:pengfei99/mlflow-pokemon-example.git -P n_estimator=15 -P max_depth=10 -P min_samples_split=2
+export MLFLOW_TRACKING_URI='http://pengfei.org:8000'
+export MLFLOW_EXPERIMENT_NAME="test-2"
+
+mlflow run git@github.com:pengfei99/mlflow-pokemon-example.git -P remote_server_uri=$MLFLOW_TRACKING_URI -P experiment_name=$MLFLOW_EXPERIMENT_NAME \
+-P data_url=https://minio.lab.sspcloud.fr/pengfei/sspcloud-demo/pokemon-partial.csv \
+-P n_estimator=15 -P max_depth=10 -P min_samples_split=2
