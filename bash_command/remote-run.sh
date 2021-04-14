7b5ede41809f73a39bccde9ed30cd31da1be463e
pengfei <liu.pengfei@hotmail.fr>
2021-04-14 18:14:11 +0200

    add new remote run script with commit-id

--- bash_command/remote-run.sh
+++ bash_command/remote-run.sh
@@ -2,5 +2,12 @@ export MLFLOW_TRACKING_URI='http://pengfei.org:8000'
 export MLFLOW_EXPERIMENT_NAME="test-2"
 
 mlflow run git@github.com:pengfei99/mlflow-pokemon-example.git -P remote_server_uri=$MLFLOW_TRACKING_URI -P experiment_name=$MLFLOW_EXPERIMENT_NAME \
--P data_url=https://minio.lab.sspcloud.fr/pengfei/sspcloud-demo/pokemon-partial.csv \
+-P data_url=https://minio.lab.sspcloud.fr/pengfei/mlflow-demo/pokemon-partial.csv \
 -P n_estimator=15 -P max_depth=10 -P min_samples_split=2
+
+
+# we can also specify a code version by using the -v commit-id.
+#mlflow run git@github.com:pengfei99/mlflow-pokemon-example.git -v 5955315f682e296e7315a567c4c142c13b21f2b0 \
+#remote_server_uri=$MLFLOW_TRACKING_URI -P experiment_name=$MLFLOW_EXPERIMENT_NAME \
+#-P data_url=https://minio.lab.sspcloud.fr/pengfei/mlflow-demo/pokemon-partial.csv \
+#-P n_estimator=15 -P max_depth=10 -P min_samples_split=2
