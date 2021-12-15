bb262bdc9cb2190ac8d0fa5ef9e770125e324d03
pengfei <liu.pengfei@hotmail.fr>
2021-12-15 11:59:20 +0100

    finish the api

--- deploy_model_with_fastapi/local_load_model.py
+++ deploy_model_with_fastapi/local_load_model.py
@@ -1,6 +1,8 @@
 import warnings
+from typing import List
 
 import joblib
+import mlflow
 import pandas as pd
 from fastapi import FastAPI
 from pydantic import BaseModel
@@ -27,27 +29,62 @@ def get_pokemon_type(hp: int, attack: int, defence: int, special_attack: int, sp
     return {"is_legendary": res}
 
 
+@pokemon_app.post("/pokemon-type")
+def post_pokemon_type(pokemon: Pokemon):
+    df = pd.DataFrame(columns=["hp", "attack", "defence", "special_attack", "special_defense", "speed"])
+    df.loc[0] = pd.Series({'hp': pokemon.hp, 'attack': pokemon.attack, 'defence': pokemon.defence,
+                           'special_attack': pokemon.special_attack,
+                           'special_defense': pokemon.special_defense, 'speed': pokemon.speed})
+    model = get_model("../models/model2.pkl")
+    res = bool(model.predict(df)[0])
+    return {"is_legendary": res}
+
+
 @pokemon_app.post("/pokemon-types")
-def get_pokemon_types(pokemon: Pokemon):
-    dic=pokemon.dict()
-    print(dic)
-    df = pd.DataFrame.from_dict(pokemon.dict())
-    df.head()
+def get_pokemon_types(pokemons: List[Pokemon]):
+    i = 0
+    df = pd.DataFrame(columns=["hp", "attack", "defence", "special_attack", "special_defense", "speed"])
+    for pokemon in pokemons:
+        df.loc[i] = pd.Series({'hp': pokemon.hp, 'attack': pokemon.attack, 'defence': pokemon.defence,
+                               'special_attack': pokemon.special_attack,
+                               'special_defense': pokemon.special_defense, 'speed': pokemon.speed})
+        i += 1
+    model = get_model("../models/model2.pkl")
+    results = []
+    for r in model.predict(df):
+        results.append(bool(r))
+    return {"is_legendary": results}
 
 
-def get_model(file_name: str):
-    with warnings.catch_warnings():
-        warnings.simplefilter("ignore", category=UserWarning)
-        model = joblib.load(open(file_name, 'rb'))
-        # pickle.load(open(file_name, 'rb'))
+def get_model(model_path: str):
+    # if model_path starts with a mlflow model path, use mlflow pyfunc to get the model
+    # mlflow model must have blow formats:
+    # - f"models:/{model_name}/{version}"
+    # - f"models:/{model_name}/{stage}"
+    if model_path.startswith("models"):
+        model = mlflow.pyfunc.load_model(
+            model_uri=model_path
+        )
+    # if it's a local fs path, use joblib to get the model
+    else:
+        with warnings.catch_warnings():
+            warnings.simplefilter("ignore", category=UserWarning)
+            model = joblib.load(open(model_path, 'rb'))
     return model
 
 
 def main():
-    result1 = get_pokemon_type(79, 115, 70, 125, 80, 111)
-    print(result1)
+    # result1 = get_pokemon_type(79, 115, 70, 125, 80, 111)
+    # print(result1)
+    full_dict = {"columns": ["hp", "attack", "defense", "special_attack", "special_defense", "speed"],
+                 "index": [272, 293, 414, 263, 49],
+                 "data": [[80, 70, 70, 90, 100, 70], [64, 51, 23, 51, 23, 28], [70, 94, 50, 94, 50, 66],
+                          [38, 30, 41, 30, 41, 60],
+                          [70, 65, 60, 90, 75, 90]]}
     p1 = Pokemon(hp=79, attack=115, defence=70, special_attack=125, special_defense=80, speed=111)
-    result2 = get_pokemon_types(p1)
+    p2 = Pokemon(hp=89, attack=115, defence=70, special_attack=125, special_defense=80, speed=111)
+    p_list = [p1, p2]
+    result2 = get_pokemon_types(p_list)
     print(result2)
 
 
