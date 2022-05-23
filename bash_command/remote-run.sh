059a5030abdfc5afe2677723371616749aac6f44
pengfei <liu.pengfei@hotmail.fr>
2022-05-23 15:45:16 +0000

    update run script

--- bash_command/remote-run.sh
+++ bash_command/remote-run.sh
@@ -1,10 +1,11 @@
 #! /bin/bash
+export MLFLOW_S3_ENDPOINT_URL='https://minio.lab.sspcloud.fr'
 export MLFLOW_TRACKING_URI='https://user-pengfei-531016.kub.sspcloud.fr/'
 export MLFLOW_EXPERIMENT_NAME="pokemon"
 
-mlflow run git@github.com:pengfei99/mlflow-pokemon-example.git -P remote_server_uri=$MLFLOW_TRACKING_URI -P experiment_name=$MLFLOW_EXPERIMENT_NAME \
+mlflow run https://github.com/pengfei99/mlflow-pokemon-example.git -P remote_server_uri=$MLFLOW_TRACKING_URI -P experiment_name=$MLFLOW_EXPERIMENT_NAME \
 -P data_url=https://minio.lab.sspcloud.fr/pengfei/mlflow-demo/pokemon-partial.csv \
--P n_estimator=15 -P max_depth=10 -P min_samples_split=2
+-P n_estimator=50 -P max_depth=30 -P min_samples_split=2
 
 
 # we can also specify a code version by using the -v commit-id.
